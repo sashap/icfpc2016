6a4e45df295cda6bf51778fb1932e38e7a06ef35
Bogdan Rybiy <bogdan.rybiy@ahrefs.com>
2016-08-04 20:33:51 -0700

    basic ops on R.t (TODO: test)

--- types.ml
+++ types.ml
@@ -14,6 +14,22 @@ let of_string s =
   match String.split s "/" with
   | exception _ -> make (atoi s) 1
   | a,b -> make (atoi a) (atoi b)
+
+let norm x y = if x.b = y.b then x,y else {a = x.a * y.b; b = x.b * y.b},{a = y.a * x.b; b = y.b * x.b} (* common denominator *)
+(* let simplify x = ? *)
+let mul x y = {a = x.a * y.a ; b = x.b * y.b}
+let add x y =
+  let x,y = norm x y in
+  assert (x.b = y.b);
+  {a = x.a + y.a; b = x.b}
+
+let sub x y =
+  let x,y = norm x y in
+  assert (x.b = y.b);
+  {a = x.a - y.a; b = x.b}
+
+let sqr x = mul x x
+let eq x y = let x,y = norm x y in x.a = y.a
 end
 
 (* point *)
@@ -42,8 +58,22 @@ let of_string s =
     Pt.of_string a, Pt.of_string b
   with
     exn -> Exn.fail ~exn "Line.of_string %S" s
+
+let length2 ((a,b) : t) = (* (x - x)^2 + (y - y)^2 no sqrt *)
+  let open Pt in
+  R.add (R.sqr (R.sub a.x b.x)) (R.sqr (R.sub a.y b.y))
+
+let is_on_line (a,b) pt =
+  R.eq (R.add (length2 (a,pt)) (length2 (pt,b))) (length2 (a,b))
 end
 
 module Problem = struct
 type t = { shape : Poly.t list; skel : Line.t list; }
+end
+
+module Solution = struct
+type source = Pt.t list
+type facets = int list
+type solution = source * facets
+type destination = Pt.t list
 end
